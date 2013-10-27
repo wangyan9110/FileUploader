0a0afc9bace661d97c46a71ac2125ae779e044e1
wangyan9110 <wangyan9110@gmail.com>
2013-10-27 22:10:08 +0800

    删除user

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 app.get('/file',file.index);
 app.get('/file/index',file.index);
 app.all('/file/uploader',file.uploader);
