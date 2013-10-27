d0ea4a99b6035c7de77efdc42a18c05f7c163ce8
wangyan9110 <wangyan9110@gmail.com>
2013-10-27 21:00:00 +0800

    文件上传引擎

--- public/javascripts/uploader/uploaderApp.js
+++ public/javascripts/uploader/uploaderApp.js
@@ -3,7 +3,7 @@
  *
  * Author:wangyan
  * Date:2013-10-27 15:41
- * Version:V1.0.0.0
+ * Version:V1.0.2.0
  * Email:yywang1991@gmail.com
  * Describe: 请描述本文件功能
  *
@@ -15,6 +15,9 @@
 
 window.uploaderApp = {};
 
+/**
+ * the simple jquery
+ */
 (function (app) {
     function App$(id) {
         return new App$.fn.init(id);
@@ -52,6 +55,9 @@ window.uploaderApp = {};
     app.$ = App$;
 })(window.uploaderApp);
 
+/**
+ *  the upload area options
+ */
 (function (app) {
 
     var _self = null;
@@ -85,157 +91,7 @@ window.uploaderApp = {};
                 });
         }
     };
-
-
     app.area = uploaderArea;
-})(window.uploaderApp);
-
-(function (app) {
-
-    var Status = {
-        Ready: 0,
-        Uploading: 1,
-        Complete: 2
-    }
-
-    var _self = null;
-
-    var instance = null;
-
-    function Queue() {
-        this._datas = [];
-        this._curSize = 0;//当前长度
-
-
-        _self = this;
-    }
-
-    Queue.prototype = {
-        add: function (data) {
-            var key = new Date().getTime();
-            this._datas.push({key: key, data: data, status: Status.Ready});
-            this._curSize = this._datas.length;
-            return key;
-        },
-        remove: function (key) {
-            var index = this._getIndexByKey(key);
-            this._datas.splice(index, 1);
-            this._curSize = this._datas.length;
-        },
-        get: function (key) {
-            var index = this._getIndexByKey(key);
-            return index != -1 ? this._datas[index].data : null;
-        },
-        clear: function () {
-            this._datas = [];
-            this._curSize = this._datas.length;
-        },
-        size: function () {
-            return this._curSize;
-        },
-        setItemStatus: function (key, status) {
-            var index = this._getIndexByKey(key);
-            if (index != -1) {
-                this._datas[index].status = status;
-            }
-        },
-        nextReadyingIndex: function () {
-            for (var i = 0; i < this._datas.length; i++) {
-                if (this._datas[i].status == Status.Ready) {
-                    return i;
-                }
-            }
-            return -1;
-        },
-        _getIndexByKey: function (key) {
-            for (var i = 0; i < this._datas.length; i++) {
-                if (this._datas[i].key == key) {
-                    return i;
-                }
-            }
-            return -1;
-        }
-    };
-
-    function getInstace() {
-        if (instance === null) {
-            instance = new Queue();
-            return instance;
-        } else {
-            return instance;
-        }
-    }
-
-
-    app.Queue = getInstace();
-    app.UploadStatus = Status;
-})(window.uploaderApp);
-
-(function (app) {
-
-    var instance = null;
-    var _self;
-
-    function uploadEngine() {
-        this._url = null;
-        this._curUploadingKey = -1;//标志
-        this.uploadStatusChanged = {};
-        _self = this;
-    }
-
-    uploadEngine.prototype = {
-        setUrl: function (url) {
-            this._url = url;
-        },
-        run: function () {
-            if (this._curUploadingKey === -1 && this._url) {
-                this._startUpload();
-            }
-        },
-        _startUpload: function () {
-            _self = this;
-            var index = app.Queue.nextReadyingIndex();
-            if (index != -1) {
-                this._uploadItem(index);
-            } else {
-                this._curUploadingKey = -1;
-                return null;
-            }
-        },
-        _uploadItem: function (index) {
-            var data = this._datas[index].data;
-            _self = this;
-            this._readyUploadItem(index);
-            var upload = uploader.send(this._url, null, data.files, function (status, data) {
-                _self._completedUploadItem.call(_self, status, data);
-            });
-            this._uploadItemProgress(upload);
-        },
-        _uploadItemProgress: function (upload) {
-            upload.onprogress = function (e) {
-                console.dir(e);
-            }
-        },
-        _readyUploadItem: function (index) {
-            this._curUploadingKey = this._datas[index].key;
-            this.uploadStatusChanged(this._curUploadingKey, app.UploadStatus.Uploading);
-            app.Queue.setItemStatus.call(this, this._curUploadingKey, app.UploadStatus.Uploading);
-        },
-        _completedUploadItem: function (status, data) {
-            this.uploadStatusChanged(this._curUploadingKey, app.UploadStatus.Complete);
-            app.Queue.setItemStatus.call(this, this._curUploadingKey, app.UploadStatus.Complete);
-            this._startUpload();
-        }
-    };
-
-    function getInstace() {
-        if (instance === null) {
-            instance = new uploadEngine();
-        }
-        return instance;
-    }
-
-    app.Engine = getInstace();
 })(window.uploaderApp);
 
 (function (app) {
---
+++ public/javascripts/uploader/uploaderQueue.js
@@ -0,0 +1,166 @@
+/**
+ * FileName:
+ *
+ * Author:wangyan
+ * Date:2013-10-27 20:50
+ * Version:V1.0.0.0
+ * Email:yywang1991@gmail.com
+ * Describe: 请描述本文件功能
+ */
+
+
+window.uploaderQueue={};
+
+/**
+ * the queue
+ */
+(function (upladerQueue) {
+
+    var Status = {
+        Ready: 0,
+        Uploading: 1,
+        Complete: 2
+    }
+
+    var _self = null;
+
+    var instance = null;
+
+    function Queue() {
+        this._datas = [];
+        this._curSize = 0;//当前长度
+
+
+        _self = this;
+    }
+
+    Queue.prototype = {
+        add: function (data) {
+            var key = new Date().getTime();
+            this._datas.push({key: key, data: data, status: Status.Ready});
+            this._curSize = this._datas.length;
+            return key;
+        },
+        remove: function (key) {
+            var index = this._getIndexByKey(key);
+            this._datas.splice(index, 1);
+            this._curSize = this._datas.length;
+        },
+        get: function (key) {
+            var index = this._getIndexByKey(key);
+            return index != -1 ? this._datas[index].data : null;
+        },
+        clear: function () {
+            this._datas = [];
+            this._curSize = this._datas.length;
+        },
+        size: function () {
+            return this._curSize;
+        },
+        setItemStatus: function (key, status) {
+            var index = this._getIndexByKey(key);
+            if (index != -1) {
+                this._datas[index].status = status;
+            }
+        },
+        nextReadyingIndex: function () {
+            for (var i = 0; i < this._datas.length; i++) {
+                if (this._datas[i].status == Status.Ready) {
+                    return i;
+                }
+            }
+            return -1;
+        },
+        _getIndexByKey: function (key) {
+            for (var i = 0; i < this._datas.length; i++) {
+                if (this._datas[i].key == key) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    };
+
+    function getInstace() {
+        if (instance === null) {
+            instance = new Queue();
+            return instance;
+        } else {
+            return instance;
+        }
+    }
+
+
+    upladerQueue.Queue = getInstace();
+    upladerQueue.UploadStatus = Status;
+})(window.uploaderQueue);
+
+/**
+ * the queue uploader engine
+ */
+(function (upladerQueue) {
+
+    var instance = null;
+    var _self;
+
+    function uploadEngine() {
+        this._url = null;
+        this._curUploadingKey = -1;//标志
+        this.uploadStatusChanged = {};
+        _self = this;
+    }
+
+    uploadEngine.prototype = {
+        setUrl: function (url) {
+            this._url = url;
+        },
+        run: function () {
+            if (this._curUploadingKey === -1 && this._url) {
+                this._startUpload();
+            }
+        },
+        _startUpload: function () {
+            _self = this;
+            var index = upladerQueue.Queue.nextReadyingIndex();
+            if (index != -1) {
+                this._uploadItem(index);
+            } else {
+                this._curUploadingKey = -1;
+                return null;
+            }
+        },
+        _uploadItem: function (index) {
+            var data = this._datas[index].data;
+            _self = this;
+            this._readyUploadItem(index);
+            var upload = uploader.send(this._url, null, data.files, function (status, data) {
+                _self._completedUploadItem.call(_self, status, data);
+            });
+            this._uploadItemProgress(upload);
+        },
+        _uploadItemProgress: function (upload) {
+            upload.onprogress = function (e) {
+                console.dir(e);
+            }
+        },
+        _readyUploadItem: function (index) {
+            this._curUploadingKey = this._datas[index].key;
+            this.uploadStatusChanged(this._curUploadingKey, upladerQueue.UploadStatus.Uploading);
+            upladerQueue.Queue.setItemStatus.call(this, this._curUploadingKey, upladerQueue.UploadStatus.Uploading);
+        },
+        _completedUploadItem: function (status, data) {
+            this.uploadStatusChanged(this._curUploadingKey, upladerQueue.UploadStatus.Complete);
+            upladerQueue.Queue.setItemStatus.call(this, this._curUploadingKey, upladerQueue.UploadStatus.Complete);
+            this._startUpload();
+        }
+    };
+
+    function getInstace() {
+        if (instance === null) {
+            instance = new uploadEngine();
+        }
+        return instance;
+    }
+
+    upladerQueue.Engine = getInstace();
+})(window.uploaderQueue);
